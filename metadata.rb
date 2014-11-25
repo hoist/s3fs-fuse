4c5df7cf82095fb08b6a76f7a6cabb3183341b0e
Owen Evans <owen@bgeek.net>
2014-11-25 14:47:11 +1300

    add uid config

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures s3fs-fuse"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.7"
+version          "0.1.8"
 
 depends 'bluepill'
 depends 'rc_mon'
